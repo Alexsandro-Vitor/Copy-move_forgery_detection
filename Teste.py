e77a8f0fcd855111ec22279cda2291c0840a0158
Alexsandro Vítor <avsc@cin.ufpe.br>
2017-11-20 14:06:26 -0300

    Otimização e implementação de variação
    * Todas as lists foram subtituidas por arrays
    * Agora é possível diferenciar orginal e cópia (embora não seja possível
    dizer qual é qual)

--- Teste.py
+++ Teste.py
@@ -40,22 +40,28 @@ def gerar_histograma(lbps, lbpMax):
 	'''
 	Gera um histograma de um conjunto de LBPs
 	'''
-	histograma = np.zeros(lbpMax + 1)
+	histograma = np.zeros(lbpMax + 1, dtype=int)
 	for lbp in lbps:
 		histograma[int(lbp)] += 1
-	return histograma / len(lbps)
+
+def num_blocos():
+	return ((rows - b) // espaco + 1) * ((columns - b) // espaco + 1) 
 
 def gerar_histograma_blocos(img, lbps, lbpMax):
 	'''
 	Gera histogramas de todos os blocos nos quais as imagens foram divididas
 	'''
-	saida = []
+	saida = np.zeros((num_blocos(), lbpMax + 1))
+	i = 0
 	for (r, c) in gerar_blocos(img):
-		lista = []
+		lista = np.zeros(b*b, dtype=int)
+		j = 0
 		for x in range(r, r+b):
 			for y in range(c, c+b):
-				lista.append(lbps[x, y])
-		saida.append(gerar_histograma(lista, lbpMax))
+				lista[j] = lbps[x, y]
+				j += 1
+		saida[i] = gerar_histograma(lista, lbpMax)
+		i += 1
 	return saida
 
 def knn(vetores, pixelsBloco):
@@ -66,11 +72,11 @@ def knn(vetores, pixelsBloco):
 
 	neigh = KNeighborsClassifier(n_neighbors=1)
 	for i in range(len(vetores)):
-		vetor = vetores[i]
+		vetor = np.copy(vetores[i])
 		vetores[i] = np.array([100 for _ in vetores[i]])
 		neigh.fit(vetores, np.array([x for x in range(len(vetores))]))
-		vetores[i] = vetor
 		saida[i] = neigh.predict([vetor])
+		vetores[i] = vetor
 	
 	return saida
 
@@ -80,27 +86,67 @@ def identificar(vA, vB, vC):
 	'''
 	saidaA = np.zeros(len(vA), dtype=bool)
 	saidaB = np.zeros(len(vA), dtype=bool)
+	saidaC = np.zeros(len(vA), dtype=bool)
+	saidaD = np.zeros(len(vA), dtype=bool)
+	
 	for i in range(len(vA)):
 		if vA[i] == vB[i] or vB[i] == vC[i] or vC[i] == vA[i]:
 			saidaA[i] = True
+			if vA[i] == vB[i]:
+				saidaB[vA[i]] = True
+			else:
+				saidaB[vC[i]] = True
 		if vA[i] == vB[i] and vA[i] == vC[i]:
-			saidaB[i] = True
-	return (saidaA, saidaB)
+			saidaC[i] = True
+			saidaD[vA[i]] = True
+	return (saidaA, saidaB, saidaC, saidaD)
 
-def editar(imgA, blocosA, blocosB):
-	imgB = np.copy(imgA)
+def identificar2(vA, vB, vC):
+	'''
+	Identifica os blocos copiados, diferenciando entre original e copia, embora nao possa dizer qual e qual
+	'''
+	saidaA = np.zeros(len(vA), dtype=bool)
+	saidaB = np.zeros(len(vA), dtype=bool)
+	saidaC = np.zeros(len(vA), dtype=bool)
+	saidaD = np.zeros(len(vA), dtype=bool)
+	
+	for i in range(len(vA)):
+		if vA[i] == vB[i] or vB[i] == vC[i] or vC[i] == vA[i]:
+			if not saidaB[i]:
+				saidaA[i] = True
+				if vA[i] == vB[i]:
+					saidaB[vA[i]] = True
+				else:
+					saidaB[vC[i]] = True
+		if vA[i] == vB[i] and vA[i] == vC[i]:
+			if not saidaD[i]:
+				saidaC[i] = True
+				saidaD[vA[i]] = True
+	return (saidaA, saidaB, saidaC, saidaD)
+
+def editar(img, blocos):
 	i = 0
-	for (r, c) in gerar_blocos(imgA):
-		if blocosA[i] == 1:
-			imgA[r:r+b, c:c+b] = [0, 0, 0]
-		if blocosB[i] == 1:
-			imgB[r:r+b, c:c+b] = [0, 0, 0]
+	saida = np.copy(img)
+	for (r, c) in gerar_blocos(img):
+		if blocos[i]:
+			saida[r:r+b, c:c+b] = [0, 0, 0]
 		i += 1
-	return (imgA, imgB)
+	return saida
+
+def editar2(img, blocosA, blocosB):
+	i = 0
+	saida = np.copy(img)
+	for (r, c) in gerar_blocos(img):
+		if blocosA[i]:
+			saida[r:r+b, c:c+b] = [255, 0, 0]
+		if blocosB[i]:
+			saida[r:r+b, c:c+b] = [0, 255, 0]
+		i += 1
+	return saida
 
 tempo = time.time()
 orig = cv2.imread("Original.bmp")
-img = cv2.imread("Modificado.bmp")
+img = cv2.imread("Original.bmp")
 cinza = rgb_para_cinza(img)
 b = 3
 espaco = 3
@@ -122,7 +168,7 @@ histogramaA = gerar_histograma_blocos(cinza, lbpA, 8 * (8-1) + 2)
 histogramaB = gerar_histograma_blocos(cinza, lbpB, 12 * (12-1) + 2)
 histogramaC = gerar_histograma_blocos(cinza, lbpC, 16 + 1)
 print("Tempo de geracao dos histogramas: " + str(time.time() - tempoHist))
-print("Num. de histogramas: " + str(len(histogramaC)))
+print("Num. de histogramas: " + str(num_blocos()))
 
 tempoKnn = time.time()
 proximosA = knn(histogramaA, b*b)
@@ -135,25 +181,28 @@ proximosC = knn(histogramaC, b*b)
 print("Tempo de calculo do KNN C: " + str(time.time() - tempoKnnC))
 print("Tempo de calculo do KNN: " + str(time.time() - tempoKnn))
 
-print(proximosA[:30])
-print(proximosB[:30])
-print(proximosC[:30])
-
 tempoId = time.time()
-(idA, idB) = identificar(proximosA, proximosB, proximosC)
+(idA, idB, idC, idD) = identificar(proximosA, proximosB, proximosC)
+(idA2, idB2, idC2, idD2) = identificar2(proximosA, proximosB, proximosC)
 print("Tempo de identificacao de copias: " + str(time.time() - tempoId))
 
-print(idA[:35])
-print(idB[:35])
-
 tempoEdit = time.time()
-(imgA, imgB) = editar(img, idA, idB)
+imgA = editar(img, idA)
+imgB = editar(img, idB)
+imgC = editar(img, idC)
+imgD = editar(img, idD)
+imgA2 = editar2(img, idA2, idB2)
+imgB2 = editar2(img, idC2, idD2)
 print("Tempo de edicao: " + str(time.time() - tempoEdit))
 
 print("Tempo de Execucao: " + str(time.time() - tempo))
 
-cv2.imshow("Imagem A", imgA)
-cv2.imshow("Imagem B", imgB)
+cv2.imshow("Copia A - min. 2", imgA)
+cv2.imshow("Copia B - min. 2", imgB)
+cv2.imshow("Copia A - min. 3", imgC)
+cv2.imshow("Copia B - min. 3", imgD)
+cv2.imshow("Copia e Original - min. 2", imgA2)
+cv2.imshow("Copia e Original - min. 3", imgB2)
 cv2.imshow("Diferenca real", orig)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
